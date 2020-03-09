959de72306a4e782a275404f4dd6aedd1888e8d0
ShashankaGifts <eegashashanka6789@gmail.com>
2020-03-09 23:17:27 +0530

    first commit

---
+++ src/main/java/designpattern1/Singleton.java
@@ -0,0 +1,27 @@
+package designpattern1;
+
+
+import java.io.*;
+import java.util.*;
+class government
+{
+	void show()
+	{
+		System.out.println("this is india government singleton class");
+	}
+}
+class card extends government
+{
+	void display()
+	{
+		System.out.println("this card is valid only in india");
+	}
+}
+public class Singleton {
+public static void main(String arg[])
+{
+	card c=new card();
+	c.show();
+	c.display();
+}
+}
